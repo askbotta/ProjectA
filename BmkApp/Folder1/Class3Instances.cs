e8b510967d8c2cf99e055f286f00f1b56383a0b0
manoj <manojbotta83@gmail.com>
2023-07-28 10:09:55 +0100

    First commit on ProjectA

---
+++ BmkApp/Folder1/Class3Instances.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmkApp.Folder1
+{
+    internal class Class3Instances
+    {
+    }
+}
